053ff882cae66ebaa1699b3f55951f08946f63b2
Adrian Villacres <jvillacresp2@est.ups.edu.ec>
2022-02-04 17:55:31 -0500

    Agregado traducciones

---
+++ MiPaint_en.ts
@@ -0,0 +1,139 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>Principal</name>
+    <message>
+        <location filename="principal.ui" line="17"/>
+        <source>Principal</source>
+        <translation>Principal</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="31"/>
+        <source>Archivo</source>
+        <translation>Archive</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="40"/>
+        <source>Estilo</source>
+        <translation>Style</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="47"/>
+        <source>Forma</source>
+        <translation>Shape</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="61"/>
+        <source>Nuevo</source>
+        <translation>New</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="64"/>
+        <source>Ctrl+N</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="69"/>
+        <source>Guardar</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="72"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="77"/>
+        <source>Salir</source>
+        <translation>Close</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="80"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="85"/>
+        <source>Color</source>
+        <translation>Color</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="88"/>
+        <source>Ctrl+Shift+C</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="93"/>
+        <source>Ancho</source>
+        <translation>Width</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="96"/>
+        <source>Ctrl+Shift+A</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="107"/>
+        <source>Lineas</source>
+        <translation>Lines</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="121"/>
+        <source>Libre</source>
+        <translation>Free</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="132"/>
+        <source>Rectánculos</source>
+        <translation>Rectangles</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="140"/>
+        <source>Circunferencias</source>
+        <translation>Circumferences</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="79"/>
+        <source>Número de líneas: </source>
+        <translation>Number of lines: </translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="140"/>
+        <source>Ancho del pincel</source>
+        <translation>Brush width</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="141"/>
+        <source>Ingrese el ancho del pincel de dibujo</source>
+        <translation>Enter the width of the brush</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="155"/>
+        <source>Color del pincel</source>
+        <translation>Color of brush</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="168"/>
+        <location filename="principal.cpp" line="174"/>
+        <location filename="principal.cpp" line="178"/>
+        <source>Guardar imagen</source>
+        <translation>Save image</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="170"/>
+        <source>Imágenes (*.png)</source>
+        <translation>Images (*.png)</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="175"/>
+        <source>Archivo almacenado en: </source>
+        <translation>File stored in: </translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="179"/>
+        <source>No se pudo almacenar la imagen.</source>
+        <translation>Couldn&apos;t store image.</translation>
+    </message>
+</context>
+</TS>
--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include "principal.h"
 
 #include <QApplication>
+#include <QTranslator>
+#include <QInputDialog>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QTranslator traduccion;
+    QStringList idiomas;
+    idiomas << "Español" << "Ingles";
+    QString idiomaSeleccionado = QInputDialog::getItem(NULL,
+                                                       "Idioma",
+                                                       "Seleccione un idioma",
+                                                       idiomas);
+    if(idiomaSeleccionado == "Ingles"){
+        traduccion.load(":/MiPaint_en.qm");
+    }
+
+    if(idiomaSeleccionado != "Español"){
+        a.installTranslator(&traduccion);
+    }
+
+
     Principal w;
     w.show();
     return a.exec();
